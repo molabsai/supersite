c7f8fb92a76c0ee794877db374a734fd960f57e0
Pasquale D'Silva <accounts@psql.me>
2021-09-02 19:31:49 -0400

    wait for doc to finish loading

--- home.js
+++ home.js
@@ -1,1 +1,10 @@
-  $(".super-navbar").after( "<img src='https://github.com/molabsai/supersite/raw/master/hero.jpg' class='hero' />" );
+if(document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded',afterDOMLoaded);
+} else {
+    afterDOMLoaded();
+}
+function afterDOMLoaded(){
+    //Everything that needs to happen after the DOM has initially loaded.
+    $(".super-navbar").after( "<img src='https://github.com/molabsai/supersite/raw/master/hero.jpg' class='hero' />" );
+
+} 
